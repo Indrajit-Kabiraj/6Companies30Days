14596ea9d45e682256f360f3c5c44eb5e0792cee
Indrajit Kabiraj <Indrajit.Kabiraj@aot.edu.in>
2022-01-04 21:44:41 +0530

    Second commit

---
+++ 12_Find total number of Squares in a NN chessboard.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        long long res = 0LL;
+        res = (N*(N+1)*(2*N+1))/6;
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
